2b2313442661dd607c3643ae014a0cbfe1abb02a
Mike <mishakozlov74@gmail.com>
2023-10-06 17:24:40 +0200

    Add reproduction code

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
-/** Add your relevant code here for the issue to reproduce */
-export default function Home() {
-  return null
+import {promises as fs} from 'fs';
+
+export default async function Home() {
+  const files = await fs.readdir([process.cwd(), 'app', 'files'].join('/'));
+
+  for (const fileName of files) {
+    const {Name} = await import(`./files/${fileName}`) as { Name: string };
+    console.log({Name, fileName})
+  }
+
+  return null;
 }
